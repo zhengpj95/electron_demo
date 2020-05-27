43634695c3a7d65056bac30bf0fdb4b1ff696eb6
ZhengAu <598359997@qq.com>
2020-05-27 23:30:17 +0800

    get todo form redis

---
+++ todoList/todo.js
@@ -0,0 +1,27 @@
+const { client, redis, smembers } = require('./connRedis');
+let todo = 'todoList';
+
+client.keys('*', redis.print)
+smembers('todoList').then(showTodoList);
+
+function showTodoList(value, err) {
+	if (err) {
+		console.log(`读取redis失败！`);
+		return;
+	}
+
+	let ul = document.getElementsByClassName('todoUl')[0];
+	if (!ul) {
+		console.log('html元素不存在');
+		return;
+	}
+
+	// 添加到html页面中
+	console.log(value, value.length);
+	for (let item of value) {
+		let li = document.createElement('li');
+		let txt = document.createTextNode(item);
+		li.appendChild(txt);
+		ul.appendChild(li);
+	}
+}
